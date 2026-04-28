bf4db22f7d5bf614da849e6bc5c7c602f48f2ea9
William Allen <134888181+Uriscythe@users.noreply.github.com>
2026-04-28 23:41:19 +0100

    Query Builder
    Combines placeholder JSON data to build a query for each possible job type

--- main.py
+++ main.py
@@ -3,3 +3,26 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+# Load your JSON file, read only.
+with open("data/search_terms.json", "r") as file:
+    #loads JSON file to Python OOP object
+    data = json.load(file)
+
+#creates 2 lists, they take their data from the JSON headers
+fields = data["fields"]
+opportunity_types = data["opportunity_types"]
+#creates new list for query results
+queries = []
+
+#nested for loop
+#for loop declares field and oppurtunity_type
+#each field in the list of fields is looped through
+for field in fields:
+    #for each field each oppurtunity type is looped through
+    for opportunity_type in opportunity_types:
+        #combines a field to oppurtunity_type together to combine a phrase
+        query = f"{field} {opportunity_type}"
+        #adds to list of queries for future use
+        queries.append(query)
+        #outputs current query to terminal
+        print(query)
