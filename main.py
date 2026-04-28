50b77c6fde0b9cedf6e0eb8873207866369f3dfe
William Allen <134888181+Uriscythe@users.noreply.github.com>
2026-04-28 23:30:10 +0100

    Deleted Demo

--- main.py
+++ main.py
@@ -3,14 +3,3 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-#finds website
-url = "http://books.toscrape.com"
-#saves response to variable
-response = requests.get(url)
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-#loops through soup variables to find a links
-for link in soup.find_all("a"):
-    #prints href destination
-    print(link.get("href"))
